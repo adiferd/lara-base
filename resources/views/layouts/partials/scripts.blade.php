5fbb9a87c362782a4b83710b227ad2805092b2e9
Adi Ferdian <adiferd@gmail.com>
2019-09-13 00:04:09 +0700

    Use CDN for JS and CSS

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,9 +1,11 @@
 <!-- Bootstrap core JavaScript-->
-<script src="{{asset('vendor')}}/jquery/jquery.min.js"></script>
-  <script src="{{asset('vendor')}}/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="{{asset('vendor')}}/jquery-easing/jquery.easing.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="{{asset('js')}}/sb-admin-2.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{asset('js')}}/sb-admin-2.min.js"></script>
